8eff850cd36129bd37b32df569df77252b651edc
MARYQA777 <marifortuna777@mail.ru>
2025-06-18 03:02:26 +0300

    First commit

--- conftest.py
+++ conftest.py
@@ -1,9 +1,8 @@
 import pytest
-from selenium import webdriver
+from selenium import  webdriver
 
 
 @pytest.fixture(scope="session")
-
 def browser():
     driver = webdriver.Chrome()
     yield driver
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,18 +1,22 @@
 from selenium.webdriver.common.by import By
 
-
-class BasePage
-
+class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = 'http://demoqa.com/'
+        self.base_url = 'https://demoqa.com/'
 
     def visit(self):
         return self.driver.get(self.base_url)
 
     def find_element(self,locator):
         return self.driver.find_element(By.CSS_SELECTOR, locator)
+
+    def url(self):
+        return self.driver.current_url
+
+
+
 
 
 
