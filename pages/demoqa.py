82672b657a1525552b45800b11ff9c051087e59e
MARYQA777 <marifortuna777@mail.ru>
2025-06-18 03:50:39 +0300

    First commit

--- pages/demoqa.py
+++ pages/demoqa.py
@@ -4,6 +4,10 @@ from pages.base_page import BasePage
 
 class DemoQa(BasePage):
 
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/'
+        super().__init__(driver, self.base_url)
+
     def exist_icon(self):
         try:
             self.find_element(locator='#app > header > a')
@@ -15,7 +19,7 @@ class DemoQa(BasePage):
         self.find_element(locator='#app > header > a').click()
 
     def equal_url(self):
-        if self.get_url() == 'https://demoqa.com/':
+        if self.get_url() == self.base_url:
            return True
         else:
            return False
---
+++ tests/test_go_to_page_elements.py
@@ -0,0 +1,4 @@
+from pages.demoqa import DemoQa
+
+
+def test_chek_icon(browser):
